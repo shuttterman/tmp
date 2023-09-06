b53e5a90ac80585886d476f199988ca14d775435
shuttterman <59496260+shuttterman@users.noreply.github.com>
2023-09-06 13:03:37 +0900

    Update xx.js

--- xx.js
+++ xx.js
@@ -1,1 +1,5 @@
-console.log("aaa")
+const execSync = require('child_process').execSync;
+// import { execSync } from 'child_process';  // replace ^ if using ES modules
+
+const output = execSync('ls', { encoding: 'utf-8' });  // the default is 'buffer'
+console.log('Output was:\n', output);
